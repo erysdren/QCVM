b850c38b45d9c5157b47d6ea906a1f361f6c37df
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-02-13 15:14:00 -0600

    Update qcvm.pas

--- bindings/pascal/qcvm.pas
+++ bindings/pascal/qcvm.pas
@@ -9,13 +9,13 @@ type
 	qcvm_t = ^longint;
 
 { main qcvm functions }
-function qcvm_open(filename : pchar) : qcvm_t; cdecl; external 'libqcvm_shared.so';
-procedure qcvm_close(qcvm : qcvm_t); cdecl; external 'libqcvm_shared.so';
-function qcvm_get_function(qcvm : qcvm_t; fname : pchar) : integer; cdecl; external 'libqcvm_shared.so';
-procedure qcvm_run(qcvm : qcvm_t; func : integer); cdecl; external 'libqcvm_shared.so';
+function qcvm_open(filename : pchar) : qcvm_t; cdecl; external 'qcvm';
+procedure qcvm_close(qcvm : qcvm_t); cdecl; external 'qcvm';
+function qcvm_get_function(qcvm : qcvm_t; fname : pchar) : integer; cdecl; external 'qcvm';
+procedure qcvm_run(qcvm : qcvm_t; func : integer); cdecl; external 'qcvm';
 
 { auxiliary qclib }
-procedure qclib_install(qcvm : qcvm_t); cdecl; external 'libqcvm_shared.so';
+procedure qclib_install(qcvm : qcvm_t); cdecl; external 'qcvm';
 
 implementation
 
